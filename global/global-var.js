c7f83d262bed02ecff969b3ec0402cf46b059d1e
surprisemochi <109345216+surprisemochi@users.noreply.github.com>
2022-07-29 18:04:57 +0200

    Added a global variables folder and file

---
+++ global/global-var.js
@@ -0,0 +1,10 @@
+// Boolean object to check if the setup is complete.
+let setupArray = {
+    target: false,
+    mandatoryRoles: false,
+    kick_time: false,
+    logs: false,
+    info: false
+};
+
+module.exports = {setupArray};
