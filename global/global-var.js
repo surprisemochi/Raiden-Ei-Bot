ef60b2285e25ffa311fb78711a064baf67062bbb
surprisemochi <109345216+surprisemochi@users.noreply.github.com>
2022-08-01 12:55:53 +0200

    Added embed

--- global/global-var.js
+++ global/global-var.js
@@ -1,3 +1,5 @@
+const { EmbedBuilder } = require('discord.js');
+
 // Boolean object to check if the setup is complete.
 let setupArray = {
     target: false,
@@ -7,4 +9,52 @@ let setupArray = {
     info: false
 };
 
-module.exports = {setupArray};
+// global
+const botVersion = 'v1.0';
+const raidenColour = 'DarkPurple';
+
+// Embeds
+function help_embed(interaction) {
+    const helpEmbed = new EmbedBuilder()
+            .setColor(`${raidenColour}`)
+            .setThumbnail(`${interaction.client.user.displayAvatarURL()}`)
+            .setTitle(`Guida all'utilizzo di ${interaction.client.user.username}.`)
+            .setDescription(`${interaction.client.user.username} si occuperà di\
+            auto-moderare i nuovi membri del server attraverso il controllo dei ruoli selezionati.\n
+            Esegui i seguenti passaggi per settare il bot:`)
+            .addFields(
+                {
+                    name: `${alertEmoji} /setup`,
+                    value: '> Con il comando \`/setup <option>\` potrai impostare il _ruolo target_,\
+                    i _ruoli obbligatori_, il _canale dei log_, il _countdown per il kick_\
+                    e potrai visualizzare il _setup corrente_ del bot.'
+                },
+                {
+                    name: `${alertEmoji} /search`,
+                    value: `> Con il comando \`/search <option>\` potrai ricercare tutti i membri del server\
+                    che non soddisfano il requisito specificato da \`<option>\`.\n> _La pulizia successiva dei\
+                    membri è facoltativa._`
+                },
+                {
+                    name: `${alertEmoji} /ping`,
+                    value: `> Per visualizzare la latenza del bot.`
+                },
+                {
+                    name: `${alertEmoji} /help`,
+                    value: `> Questa pagina!`
+                },
+                {
+                    name: `${successEmoji} Fine!`,
+                    value: `> Il bot è pronto per l'utilizzo.`
+                },
+            )
+            .setFooter(
+                {
+                    text: `${botVersion} | Developed with ❤️ by ${selfProfile.tag}\n⬇️ Clicca i bottoni per maggiori informazioni sui comandi ⬇️`,
+                    iconURL: `${selfProfile.displayAvatarURL()}`
+                }
+            )
+    return helpEmbed;
+}
+
+module.exports = {setupArray, botVersion, raidenColour, help_embed};
