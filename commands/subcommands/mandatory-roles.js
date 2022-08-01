45116132ec8efe975f2f2a86f765848ccd0e3328
surprisemochi <109345216+surprisemochi@users.noreply.github.com>
2022-08-01 13:38:18 +0200

    Improved how the roles are received and displayed.

--- commands/subcommands/mandatory-roles.js
+++ commands/subcommands/mandatory-roles.js
@@ -3,25 +3,26 @@ const { EmbedBuilder } = require('discord.js');
 const subcmd_mandRoles = (interaction) => {
     
     // Pre-increment because of the Option's names. (See interaction).
-    for(let i = 0; i < 5; i++) {
-        mandRole[i] = interaction.options.getRole(`ruolo${++i}`);
-        i--;
-        console.log('New Mandatory role have been set:');
+    for(let i = 1; i <= 6; i++) {
+        mandRole.push(interaction.options.getRole(`ruolo${i}`));
     }
-    console.log(mandRole);
+    console.log({mandRole});
+    console.log(`Length: ${Object.keys(mandRole).length}`);
+
+    const roleString = mandRole.join(' ');
 
     const mandRoleEmbed = new EmbedBuilder()
         .setColor(`${raidenColour}`)
         .setTitle(`${successEmoji} Fatto!`)
         .addFields(
-            {name: 'Ruoli Obbligatori:', value: `${mandRole[0]}\n${mandRole[1]}\n${mandRole[2]}\n${mandRole[3]}\n${mandRole[4]}\n`}
+            {name: 'Ruoli impostati:', value: `${roleString}`}
         )
         .setTimestamp()
         .setFooter(
             {text: `Developed with ❤️ by ${selfProfile.tag}`,
             iconURL: `${selfProfile.displayAvatarURL()}`}
         )
-    
+
     return interaction.reply({embeds: [mandRoleEmbed]});
 }
 
