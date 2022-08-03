b3ab59d567ad9575e41808662ec1ff7c295eb3d6
surprisemochi <109345216+surprisemochi@users.noreply.github.com>
2022-08-03 17:08:48 +0200

    Removed old unecessary code.

--- index.js
+++ index.js
@@ -51,15 +51,4 @@ for (const file of eventFiles) {
 	}
 }
 
-// Global variables used throughout the program.
-// global.targetRole, global.logChannel = null;
-// global.kickTime = 60000;
-// global.mandRole = new Array();
-
-// global.raidenColour = 'DarkPurple';
-// global.successEmoji = '<a:bot_success:522080656604397591>';
-// global.alertEmoji = '<a:bot_alert:997448756398129222>';
-// global.warningEmoji = '<:bot_warning:994288250858508369>';
-// global.typingEmoji = '<a:bot_typing:1002938798067417159>';
-
 client.login(token);
